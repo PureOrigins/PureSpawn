327a54fd56e7a95749b2a11748dc50084d56918c
Raymark97 <raymark.ra@gmail.com>
2021-09-26 18:31:46 +0200

    added safety check

--- src/main/kotlin/it/pureorigins/randomspawn/RandomSpawn.kt
+++ src/main/kotlin/it/pureorigins/randomspawn/RandomSpawn.kt
@@ -3,6 +3,7 @@ package it.pureorigins.randomspawn
 import net.fabricmc.api.ModInitializer
 import net.fabricmc.fabric.api.networking.v1.PacketSender
 import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents
+import net.minecraft.block.Blocks
 import net.minecraft.server.MinecraftServer
 import net.minecraft.server.network.ServerPlayNetworkHandler
 import net.minecraft.stat.Stats
@@ -15,12 +16,21 @@ object RandomSpawn : ModInitializer {
 
     override fun onInitialize() {
         ServerPlayConnectionEvents.JOIN.register { handler: ServerPlayNetworkHandler, _: PacketSender, _: MinecraftServer ->
-            if (handler.player.statHandler.getStat(Stats.CUSTOM.getOrCreateStat(Stats.PLAY_TIME)) == 0) {
-                val x = Random.nextInt(RANGE)
-                val y = Random.nextInt(RANGE)
-                //TODO safety check, we care about our players health
-                val block = handler.player.world.getBlockEntity(BlockPos(x,y,64))
-                handler.player.teleport(handler.player.serverWorld, x.toDouble(), y.toDouble(),64.0, 0.0F,0.0F)
+            val p = handler.player
+            if (p.statHandler.getStat(Stats.CUSTOM.getOrCreateStat(Stats.PLAY_TIME)) == 0) {
+
+                var x = 0
+                var y = 0
+                var z = 64
+                var b = p.world.getBlockState(BlockPos(x, y, 0))
+
+                while (b === Blocks.WATER) {
+                    x = Random.nextInt(RANGE)
+                    y = Random.nextInt(RANGE)
+                    b = p.world.getBlockState(BlockPos(x, y, z))
+                }
+                while(b !== Blocks.AIR) b = p.world.getBlockState(BlockPos(x, y, z++))
+                p.teleport(p.serverWorld, x.toDouble(), y.toDouble(), z.toDouble(),0.0F,0.0F)
             }
         }
     }
