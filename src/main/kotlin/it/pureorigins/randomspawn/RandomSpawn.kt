ce684a17f437f0a80b3be71f154b854b7146d4e4
Raymark97 <raymark.ra@gmail.com>
2021-10-12 11:37:51 +0200

    -players now spawns in a safe place -added centerX and centerZ to config -TODO avoid spawning on trees

--- src/main/kotlin/it/pureorigins/randomspawn/RandomSpawn.kt
+++ src/main/kotlin/it/pureorigins/randomspawn/RandomSpawn.kt
@@ -9,13 +9,18 @@ import net.fabricmc.api.ModInitializer
 import net.fabricmc.fabric.api.command.v1.CommandRegistrationCallback
 import net.fabricmc.fabric.api.networking.v1.PacketSender
 import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents
+import net.minecraft.block.BlockState
 import net.minecraft.block.Blocks
+import net.minecraft.block.Blocks.*
 import net.minecraft.server.MinecraftServer
 import net.minecraft.server.command.CommandManager.literal
 import net.minecraft.server.command.ServerCommandSource
 import net.minecraft.server.network.ServerPlayNetworkHandler
 import net.minecraft.stat.Stats
 import net.minecraft.util.math.BlockPos
+import net.minecraft.world.World
+import org.apache.logging.log4j.Level.INFO
+import org.apache.logging.log4j.LogManager
 import java.lang.Math.toRadians
 import kotlin.math.cos
 import kotlin.math.roundToInt
@@ -25,55 +30,96 @@ import kotlin.random.Random
 
 object RandomSpawn : ModInitializer {
 
+    private val logger = LogManager.getLogger()
+    //TODO avoid spawning on trees
     override fun onInitialize() {
 
         val config = json.readFileAs(configFile("randomspawn.json"), Config())
 
-        ServerPlayConnectionEvents.JOIN.register { handler: ServerPlayNetworkHandler, _: PacketSender, _: MinecraftServer ->
+        ServerPlayConnectionEvents.JOIN.register { handler: ServerPlayNetworkHandler, _: PacketSender, server: MinecraftServer ->
             val p = handler.player
             if (p.statHandler.getStat(Stats.CUSTOM.getOrCreateStat(Stats.PLAY_TIME)) == 0) {
 
-                var x = 0
+                var x: Int
                 var y = 64
-                var z = 0
-                var b = p.world.getBlockState(BlockPos(x, 0, z))
+                var z: Int
+                var b: BlockState
+                var pos: BlockPos
 
-                while (b === Blocks.WATER) {
+                do {
                     val radius = Random.nextInt(config.range)
                     val angle = toRadians(Random.nextDouble(360.0))
                     x = (radius * cos(angle)).roundToInt()
                     z = (radius * sin(angle)).roundToInt()
-                    b = p.world.getBlockState(BlockPos(x, y, z))
+                    pos = BlockPos(x+config.centerX, y - 1, z+config.centerZ)
+                    b = p.world.getBlockState(pos)
+                    logger.info("Selected block $x $y $z - ${b.block}")
+                } while (!isGroundOk(pos, p.world))
+                while (!isSpaceOk(pos, p.world)) {
+                    pos=BlockPos(x, ++y, z)
+                    b = p.world.getBlockState(pos)
+                    logger.info("Selected upper block $x $y $z - ${b.block}")
                 }
-                while (b !== Blocks.AIR) b = p.world.getBlockState(BlockPos(x, ++y, z))
-                p.setSpawnPoint(p.world.registryKey, BlockPos(x, y, z), 90F, true, false)
+                p.setSpawnPoint(p.world.registryKey, pos, 0F, true, false)
             }
         }
 
-        CommandRegistrationCallback.EVENT.register(CommandRegistrationCallback { dispatcher: CommandDispatcher<ServerCommandSource?>, dedicated: Boolean ->
+        CommandRegistrationCallback.EVENT.register(CommandRegistrationCallback { dispatcher: CommandDispatcher<ServerCommandSource?>, _: Boolean ->
             dispatcher.register(literal("test").executes { context ->
                 val p = context.source.player
-                var x = 0
+                var x: Int
                 var y = 64
-                var z = 0
-                var b = p.world.getBlockState(BlockPos(x, 0, z))
+                var z: Int
+                var b: BlockState
+                var pos: BlockPos
 
-                while (b === Blocks.WATER) {
+                do {
                     val radius = Random.nextInt(config.range)
                     val angle = toRadians(Random.nextDouble(360.0))
                     x = (radius * cos(angle)).roundToInt()
                     z = (radius * sin(angle)).roundToInt()
-                    b = p.world.getBlockState(BlockPos(x, y, z))
+                    pos = BlockPos(x+config.centerX, y - 1, z+config.centerZ)
+                    b = p.world.getBlockState(pos)
+                    logger.info("Selected block $x $y $z - ${b.block}")
+                } while (!isGroundOk(pos, p.world))
+                while (!isSpaceOk(pos, p.world)) {
+                    pos=BlockPos(x, ++y, z)
+                    b = p.world.getBlockState(pos)
+                    logger.info("Selected upper block $x $y $z - ${b.block}")
                 }
-                while (b !== Blocks.AIR) b = p.world.getBlockState(BlockPos(x, ++y, z))
-                p.setSpawnPoint(p.world.registryKey, BlockPos(x, y, z), 90F, true, false)
+                p.setSpawnPoint(p.world.registryKey, pos, 90F, true, false)
+                p.teleport(p.serverWorld, x.toDouble(), y.toDouble(), z.toDouble(), 0F, 0F)
                 1
             })
         })
     }
 
+    private fun isGroundOk(pos: BlockPos, w: World): Boolean {
+        val ground = w.getBlockState(BlockPos(pos.x, pos.y - 1, pos.z))
+
+        if (ground.block === WATER || ground.block === LAVA || ground.block === CACTUS) {
+            logger.info("Dangerous block: ${pos.x} ${pos.y - 1} ${pos.z} is on the ${ground.block}")
+            return false
+        }
+        logger.info("Selected block ${pos.x} ${pos.y - 1} ${pos.z} is not a dangerous terrain")
+        return true
+    }
+
+    private fun isSpaceOk(pos: BlockPos, w: World): Boolean {
+        val head = w.getBlockState(BlockPos(pos.x, pos.y + 1, pos.z))
+        val legs = w.getBlockState(BlockPos(pos.x, pos.y, pos.z))
+        if (head.block !== AIR || legs.block !== AIR) {
+            logger.info("Selected block ${pos.x} ${pos.y} ${pos.z} is underground")
+            return false
+        }
+        logger.info("Selected block ${pos.x} ${pos.y} ${pos.z} does not obstruct player")
+        return true
+    }
+
     @Serializable
     data class Config(
-        val range: Int = 0
+        val range: Int = 0,
+        val centerX: Int = 0,
+        val centerZ: Int = 0
     )
 }
