48062334c62c6ffe6e5dc54073c0d5b49bdbee1c
Raymark97 <marcoliga97@gmai.com>
2021-10-19 12:53:34 +0200

    implemented SpawnBuffer

--- src/main/kotlin/it/pureorigins/randomspawn/RandomSpawn.kt
+++ src/main/kotlin/it/pureorigins/randomspawn/RandomSpawn.kt
@@ -15,6 +15,7 @@ import net.minecraft.server.command.CommandManager.literal
 import net.minecraft.server.command.ServerCommandSource
 import net.minecraft.server.network.ServerPlayNetworkHandler
 import net.minecraft.server.network.SpawnLocating
+import net.minecraft.server.world.ServerWorld
 import net.minecraft.stat.Stats
 import net.minecraft.util.math.BlockPos
 import org.apache.logging.log4j.LogManager
@@ -25,51 +26,34 @@ import java.util.*
 object RandomSpawn : ModInitializer {
 
     private val logger = LogManager.getLogger()
-    private var spawnBuffer = Stack<BlockPos>()
+    private lateinit var spawnBuffer: SpawnBuffer
 
     //TODO avoid spawning on trees
     override fun onInitialize() {
         val config = json.readFileAs(configFile("randomspawn.json"), Config())
         logger.info("RandomSpawn successfully loaded!")
         ServerLifecycleEvents.SERVER_STARTED.register {
-
-            for(i in 0..config.spawnBufferSize) {
-                var pos: BlockPos?
-                do {
-                    val chunkPos = it.overworld.getChunk(randomPos(config.range, config.centerX, config.centerZ)).pos
-                    pos = SpawnLocating.findServerSpawnPoint(it.overworld, chunkPos, true)
-                    print("Spawn $i: $pos")
-                } while (pos == null)
-                spawnBuffer.push(pos)
-            }
+            spawnBuffer =
+                SpawnBuffer(it.overworld, config.range, config.centerX, config.centerZ, config.spawnBufferSize)
         }
 
         ServerPlayConnectionEvents.JOIN.register { handler: ServerPlayNetworkHandler, _: PacketSender, _: MinecraftServer ->
             val p = handler.player
             if (p.statHandler.getStat(Stats.CUSTOM.getOrCreateStat(Stats.PLAY_TIME)) == 0) {
-
-                var pos: BlockPos?
-
-                do {
-                    val chunkPos = p.world.getChunk(randomPos(config.range, config.centerX, config.centerZ)).pos
-                    pos = SpawnLocating.findServerSpawnPoint(p.serverWorld, chunkPos, true)
-                } while (pos == null)
-                p.teleport(pos!!.x + 0.5, pos!!.y.toDouble(), pos!!.z + 0.5)
+                val pos = spawnBuffer.pop()
+                p.teleport(pos.x + 0.5, pos.y.toDouble(), pos.z + 0.5)
                 p.setSpawnPoint(p.world.registryKey, pos, 90F, true, false)
+                spawnBuffer.addSpawnPoint()
             }
         }
 
         CommandRegistrationCallback.EVENT.register(CommandRegistrationCallback { dispatcher: CommandDispatcher<ServerCommandSource?>, _: Boolean ->
             dispatcher.register(literal("test").executes { context ->
                 val p = context.source.player
-                var pos: BlockPos?
-
-                do {
-                    val chunkPos = p.world.getChunk(randomPos(config.range, config.centerX, config.centerZ)).pos
-                    pos = SpawnLocating.findServerSpawnPoint(p.serverWorld, chunkPos, true)
-                } while (pos == null)
-                p.teleport(pos!!.x + 0.5, pos!!.y.toDouble(), pos!!.z + 0.5)
+                val pos = spawnBuffer.pop()
+                p.teleport(pos.x + 0.5, pos.y.toDouble(), pos.z + 0.5)
                 p.setSpawnPoint(p.world.registryKey, pos, 90F, true, false)
+                spawnBuffer.addSpawnPoint()
                 1
             })
         })
@@ -93,4 +77,33 @@ object RandomSpawn : ModInitializer {
         val centerZ: Int = 0,
         val spawnBufferSize: Int = 10
     )
+
+    class SpawnBuffer(
+        private val overworld: ServerWorld,
+        private val range: Int,
+        private val centerX: Int,
+        private val centerZ: Int,
+        size: Int
+    ) : Stack<BlockPos>() {
+        init {
+            for (i in 0..size) this.push(findSpawnPoint())
+        }
+
+        override fun pop(): BlockPos = if (empty())
+            findSpawnPoint()
+        else super.pop()
+
+        fun addSpawnPoint() {
+            push(findSpawnPoint())
+        }
+
+        private fun findSpawnPoint(): BlockPos {
+            var pos: BlockPos?
+            do {
+                val chunkPos = overworld.getChunk(randomPos(range, centerX, centerZ)).pos
+                pos = SpawnLocating.findServerSpawnPoint(overworld, chunkPos, true)
+            } while (pos == null)
+            return pos
+        }
+    }
 }
